98edbaaa2bd6e3c9ea5ef7fa6df416cf30bbde32
Amro Abdelkamel <amromoorie@gmail.com>
2022-03-15 23:49:50 +0200

    Major refactor: - replaced create-react-app with vite - bootstarp cdn with tailwind - redux with redux toolkit
    - removed lodash for bundle saving
    - project cleanup removing uncessary files like fake pages
    - project to prepare for simply viewing a collections of dogs by breeds as a starting point

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-gray-200 h-screen flex justify-center">
+      hello world
+    </div>
+  )
+}
+
+export default App
--- src/hooks/usePagination.jsx
+++
@@ -1,6 +0,0 @@
-import _ from 'lodash';
-
-export const paginate = (collection, current, pageSize) => {
-  const start = (current - 1) * pageSize;
-  return _(collection).slice(start).take(pageSize).value();
-};
