00d212c911f0201e2face88052cd862fcc19da8f
Amro Abdelkamel <amromoorie@gmail.com>
2022-03-21 23:49:06 +0200

    Feat(search by selected breed) & Styles

--- src/components/DogCollection/DogCollectionBreeds.jsx
+++ src/components/DogCollection/DogCollectionBreeds.jsx
@@ -9,55 +9,67 @@ import DogCollectionBreedsGallery from './DogCollectionBreedsGallery'
 // import { useFetch } from '../../hooks/useFetch';
 
 export default function DogCollectionBreeds() {
-  const handleClick = (e) => {
-    return null
-  }
+ 
+  const [selectBreed, setSelectBreed] = useState('')
 
   console.log('data: ', useFetchDogBreedsQuery())
-  const { data: breeds = [] } = useFetchDogBreedsQuery()
-  const { data: breedsSelected = '' } = useFetchDogBreedsSelectedQuery()
+  const { data: breeds , isLoading, isSuccess, isError, isFetching  } = useFetchDogBreedsQuery()
+  const { data: breedsSelected = '', isLoading: selectedLoading, isSuccess: selectedSuccess, isError: selectedError, isFetching: selectedFetched } = useFetchDogBreedsSelectedQuery(selectBreed)
+  console.log('breedsSelectedDATA: ', breedsSelected);
+  // console.log('breeds: ', breeds.message);
 
-  // const [selectBreed, setSelectBreed] = useState('')
 
   const [currentPage, setCurrentPage] = useState(1)
   const [pageSize, setPageSize] = useState(6)
   // const [collection, setCollection] = useState(null);
 
   const handlePageChange = (page) => setCurrentPage(page)
+  const handleClick =  (e) =>{
+    console.log("e", e.target.dataset.selectedBreed)
+    setSelectBreed(e.target.dataset.selectedBreed)
+  }
 
-  const imgCount = breeds.length
-  console.log('count', imgCount)
+  // const imgCount = breeds.length
+  // console.log('count', imgCount)
   // let imgSrcs = breedsSelected.map((src) => src.url)
   // console.log('imgSrcs:::@', imgSrcs);
 
   // const gallery = usePagination(imgSrcs, currentPage, pageSize)
   // console.log('gallery: ', gallery)
-  const gallery = ''
+
+  if (isFetching) return <h3 className='pl-2'>loading...</h3>
+  if (selectedFetched) return <h3 className='pl-2'>loading...</h3>
 
   return (
     <>
-      <header>
-        <h2>Choose a dog breed</h2>
-        <ul className='space-x-4'>
-          {Array.isArray(breeds) &&
-            breeds.map((li, idx) => (
+      <header className='mt-8 mx-20 flex justify-between'>
+        <h2 className='text-3xl py-2 text-cyan-600 font-semibold '>Choose a dog breed</h2>
+
+
+        <ul className='cursor-pointer w-[70%] h-24   space-x-4 space-y-2  overflow-x-hidden '> 
+          {Array.isArray(breeds.message) &&
+            breeds.message.map((data, idx) => (
               <li
                 key={idx}
-                className='inline-flex px-4 py-2 rounded-full text-secondary bg-accent  font-semibold text-sm  align-center w-max'
-                onClick={handleClick}
+                className=' text-white flex-shrink-0 inline-block px-2 py-1 rounded-full text-secondary bg-cyan-700  font-semibold text-sm  align-center'
+                onClick={ handleClick}
+                data-selected-breed={data}
               >
-                {li}
+                {data}
               </li>
             ))}
-        </ul>
+         </ul> 
+
+
+
       </header>
       {/* <DogCollectionBreedsGallery imgSrcs={gallery} /> */}
-      <Pagination
+      {/* <Pagination
         collection={imgCount}
         pageSize={pageSize}
         onPageChange={handlePageChange}
         currentPage={currentPage}
-      />
+      /> */}
     </>
   )
 }
