ca5cfeb7239c6ed6aac6f5efc214147a67602bc5
Amro Abdelkamel <amromoorie@gmail.com>
2022-05-08 20:47:19 +0200

    Feat: prepare modal for individual card

--- src/components/DogCollection/DogCollectionBreedsModal.jsx
+++ src/components/DogCollection/DogCollectionBreedsModal.jsx
@@ -6,19 +6,44 @@ export default function DogCollectionBreedsModal({
   return (
     <>
       {toggleModal && (
-        <div className="">
+        // <div className=''>
+        <>
           <div
-           
-            className='fixed inset-0 bg-black opacity-90 z-10   '
+            onClick={() => setToggleModal(!toggleModal)}
+            className='z-[200] fixed inset-0'
           ></div>
-          <div  onClick={() => setToggleModal(!toggleModal)} className='z-10 fixed inset-0 flex justify-center items-center   '>
-            <div className='z-20 inset-0 relative bg-gray-500  objectd-contain '>
-              <figure className='z-30'>
-                <img className='object-cover h-[15rem] w-[20rem] sm:h-72 sm:w-96' src={breedsSelectedImg} alt='image of a dog breed' />
-              </figure>
-            </div>
+          <div className='fixed inset-0 bg-black opacity-90'></div>
+          <div className='justify-center items-center reladtive'>
+            {/* <div   className='z-[100] inset-0 absolute bg-gray-500  objectd-contain '> */}
+            <figure className='pointer-events-none absolute left-[50%] ml-[-11rem] mx-auto top-[50%] mt-[-7.5rem] z-[300]'>
+              <img
+                className='pointer-events-none rounded-lg object-cover h-[15rem] w-[22rem] sm:h-72 sm:w-96'
+                src={breedsSelectedImg}
+                alt='image of a dog breed'
+              />
+            </figure>
+            {/* </div> */}
           </div>
-        </div>
+
+          {/* Close Button */}
+          <div className='text-white fixed bottom-10 border-2 rounded-full p-5 '>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-6 w-6'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M6 18L18 6M6 6l12 12'
+              />
+            </svg>
+          </div>
+          {/* </div> */}
+        </>
       )}
     </>
   )
